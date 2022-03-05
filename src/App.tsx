8614a33c3751434a86876ef8f7b3ecc60d6438f2
joel-samuel-raj <80976002+joel-samuel-raj@users.noreply.github.com>
2022-03-06 02:17:59 +0530

    configured mui

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,11 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p> Lorem ipsum dolor sit amet consectetur, adipisicing elit. Explicabo impedit ut molestias debitis tempora, eum odit modi dicta quos dolor. Facere, voluptatum ad? Veritatis minima commodi id accusamus quos a adipisci alias ea officia eius tempore doloribus labore earum assumenda aspernatur placeat, eum cumque! Voluptatibus optio in magni laboriosam, amet dignissimos ipsam quod totam dolor, mollitia quas recusandae illo est inventore repellendus eos doloremque quam nemo numquam perspiciatis veritatis. Accusantium fuga qui laborum blanditiis sequi modi officiis dignissimos, amet distinctio assumenda porro, in eaque, earum soluta? Vel tempore libero ratione porro quo accusamus, dolores incidunt aut ab expedita culpa optio? </p>
     </div>
   );
 }
